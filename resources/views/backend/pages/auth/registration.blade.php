ff477584e57e43a4b929201e6dffb8bcd4d72e14
zidankhan86 <zidankhan718@gmail.com>
2024-07-30 22:56:04 +0600

    fix: login , logout , registration and profile

--- resources/views/backend/pages/auth/registration.blade.php
+++ resources/views/backend/pages/auth/registration.blade.php
@@ -2,118 +2,42 @@
 @extends('frontend.master')
 
   @section('content')
-  <!-- breedcrumb section start  -->
-  <div class="section breedcrumb">
-    <div class="breedcrumb__img-wrapper">
-      <img src="{{asset ('frontend/src/images/banner/breedcrumb.jpg') }}" alt="breedcrumb" />
-      <div class="container">
-        <ul class="breedcrumb__content">
-          <li>
-            <a href="#">
-              <svg
-                width="18"
-                height="19"
-                viewBox="0 0 18 19"
-                fill="none"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <path
-                  d="M1 8L9 1L17 8V18H12V14C12 13.2044 11.6839 12.4413 11.1213 11.8787C10.5587 11.3161 9.79565 11 9 11C8.20435 11 7.44129 11.3161 6.87868 11.8787C6.31607 12.4413 6 13.2044 6 14V18H1V8Z"
-                  stroke="#808080"
-                  stroke-width="1.5"
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                />
-              </svg>
-              <span> > </span>
-            </a>
-          </li>
-          <li class="active">
-            <a href="create-account.html">Create Account</a>
-          </li>
-        </ul>
-      </div>
-    </div>
-  </div>
-  <!-- breedcrumb section end   -->
-
-  <!-- create account-in Section Start  -->
-  <section class="create-account section section--xl">
+ 
+  <section class="signin-page account">
     <div class="container">
-      <div class="form-wrapper">
-        <h6 class="font-title--sm">create account</h6>
-        <form action="{{route('registration.submit')}}" method="POST" enctype="multipart/form-data">
-            @csrf
-            <div class="form-input">
-                <input type="text" class="input" name="name" value="{{old('name')}}" placeholder="Name" />
-                @error('email')
-                <p class="text-danger">{{$message}}</p>
-                @enderror
+      <div class="row">
+        <div class="col-md-6 col-md-offset-3">
+          <div class="block text-center">
+            <a class="logo" href="index.html">
+              <img src="images/logo.png" alt="">
+            </a>
+            <h2 class="text-center">Create Your Account</h2>
+            <form action="{{route('registration.submit')}}" method="POST" enctype="multipart/form-data">
+              @csrf
+              <div class="form-group">
+                <input type="text" class="form-control" name="name"  placeholder="Name">
               </div>
-
-          <div class="form-input">
-            <input type="email" class="input" name="email" value="{{old('email')}}" placeholder="Email" />
-            @error('email')
-            <p class="text-danger">{{$message}}</p>
-            @enderror
+              <div class="form-group">
+                <input type="text" class="form-control" name="address"  placeholder="Address">
+              </div>
+              <div class="form-group">
+                <input type="tell" class="form-control" name="phone"  placeholder="Phone Number">
+              </div>
+              <div class="form-group">
+                <input type="email" class="form-control" name="email"  placeholder="Email">
+              </div>
+              <div class="form-group">
+                <input type="password" class="form-control" name="password"  placeholder="Password">
+              </div>
+              <div class="text-center">
+                <button type="submit" class="btn btn-main text-center">Sign In</button>
+              </div>
+            </form>
+            <p class="mt-20">Already hava an account ?<a href="{{ url('login-frontend') }}"> Login</a></p>
+        
           </div>
-          <div class="form-input">
-            <input type="password" placeholder="Password" name="password" id="password" />
-            <button
-              type="button"
-              class="icon icon-eye"
-              onclick="showPassword('password',this)"
-            >
-              <svg
-                width="20"
-                height="21"
-                viewBox="0 0 20 21"
-                fill="none"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <path
-                  d="M1.66663 10.5003C1.66663 10.5003 4.69663 4.66699 9.99996 4.66699C15.3033 4.66699 18.3333 10.5003 18.3333 10.5003C18.3333 10.5003 15.3033 16.3337 9.99996 16.3337C4.69663 16.3337 1.66663 10.5003 1.66663 10.5003Z"
-                  stroke="currentColor"
-                  stroke-width="1.5"
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                />
-                <path
-                  d="M10 13C10.663 13 11.2989 12.7366 11.7678 12.2678C12.2366 11.7989 12.5 11.163 12.5 10.5C12.5 9.83696 12.2366 9.20107 11.7678 8.73223C11.2989 8.26339 10.663 8 10 8C9.33696 8 8.70107 8.26339 8.23223 8.73223C7.76339 9.20107 7.5 9.83696 7.5 10.5C7.5 11.163 7.76339 11.7989 8.23223 12.2678C8.70107 12.7366 9.33696 13 10 13V13Z"
-                  stroke="currentColor"
-                  stroke-width="1.5"
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                />
-              </svg>
-            </button>
-            @error('password')
-            <p class="text-danger">{{$message}}</p>
-            @enderror
-          </div>
-         
-
-          <div class="form-input">
-            <input type="text" class="input" name="address" value="{{old('address')}}" placeholder="Address" />
-            @error('address')
-            <p class="text-danger">{{$message}}</p>
-            @enderror
-          </div>
-
-          <div class="form-input">
-            <input type="number" class="input" name="phone" value="{{old('phone')}}" placeholder="Phone" />
-            
-          </div>
-          <div class="form-button">
-            <button type="submit" class="button button--md w-100">Create Account</button>
-          </div>
-          <div class="form-register">
-            Already have account ? <a href="{{ url('login-frontend') }}">Login</a>
-          </div>
-        </form>
+        </div>
       </div>
     </div>
   </section>
-  <!-- create account-in Section end  -->
-
   @endsection
--- resources/views/frontend/components/profile.blade.php
+++ resources/views/frontend/components/profile.blade.php
@@ -1,161 +1,55 @@
 
-<!-- dashboard Secton Start  -->
-<div class="dashboard section">
+
+
+
+  <section class="page-header">
     <div class="container">
-      <div class="row dashboard__content">
-        <div class="col-lg-3">
-          <nav class="dashboard__nav">
-            <h5 class="dashboard__nav-title font-body--xxl-500">
-              navigation
-            </h5>
-            <ul class="dashboard__nav-item">
-              <li class="dashboard__nav-item-link active">
-                <a href="user-dashboard.html" class="font-body--lg-400">
-                  <span class="icon">
-                    <svg
-                      width="24"
-                      height="24"
-                      viewBox="0 0 24 24"
-                      fill="none"
-                      xmlns="http://www.w3.org/2000/svg"
-                    >
-                      <path
-                        d="M21 21H13V15H21V21ZM11 21H3V11H11V21ZM21 13H13V3H21V13ZM11 9H3V3H11V9Z"
-                        fill="currentColor"
-                      />
-                    </svg>
-                  </span>
-                  <span class="name">Dashboard</span>
-                </a>
-              </li>
-            
-            
-              <!--  Log out    -->
-              <li class="dashboard__nav-item-link">
-                <a href="{{ route('logout') }}" class="font-body--lg-400">
-                  <span class="icon">
-                    <svg
-                      width="24"
-                      height="24"
-                      viewBox="0 0 24 24"
-                      fill="none"
-                      xmlns="http://www.w3.org/2000/svg"
-                    >
-                      <path
-                        d="M19 21H10C9.46957 21 8.96086 20.7893 8.58579 20.4142C8.21071 20.0391 8 19.5304 8 19V15H10V19H19V5H10V9H8V5C8 4.46957 8.21071 3.96086 8.58579 3.58579C8.96086 3.21071 9.46957 3 10 3H19C19.5304 3 20.0391 3.21071 20.4142 3.58579C20.7893 3.96086 21 4.46957 21 5V19C21 19.5304 20.7893 20.0391 20.4142 20.4142C20.0391 20.7893 19.5304 21 19 21ZM12 16V13H3V11H12V8L17 12L12 16Z"
-                        fill="currentColor"
-                      />
-                    </svg>
-                  </span>
-
-                  <span class="name"> Log out </span>
-                  
-                </a>
-              </li>
-            </ul>
-            <button class="filter-icon">
-              <svg
-                width="22"
-                height="19"
-                viewBox="0 0 22 19"
-                fill="none"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <path
-                  d="M18 5.75C18.4142 5.75 18.75 5.41421 18.75 5C18.75 4.58579 18.4142 4.25 18 4.25V5.75ZM9 4.25C8.58579 4.25 8.25 4.58579 8.25 5C8.25 5.41421 8.58579 5.75 9 5.75V4.25ZM18 4.25H9V5.75H18V4.25Z"
-                  fill="white"
-                />
-                <path
-                  d="M13 14.75C13.4142 14.75 13.75 14.4142 13.75 14C13.75 13.5858 13.4142 13.25 13 13.25V14.75ZM4 13.25C3.58579 13.25 3.25 13.5858 3.25 14C3.25 14.4142 3.58579 14.75 4 14.75V13.25ZM13 13.25H4V14.75H13V13.25Z"
-                  fill="white"
-                />
-                <circle
-                  cx="5"
-                  cy="5"
-                  r="4"
-                  stroke="white"
-                  stroke-width="1.5"
-                />
-                <circle
-                  cx="17"
-                  cy="14"
-                  r="4"
-                  stroke="white"
-                  stroke-width="1.5"
-                />
-              </svg>
-            </button>
-          </nav>
+      <div class="row">
+        <div class="col-md-12">
+          <div class="content">
+            <h1 class="page-name">Dashboard</h1>
+            <ol class="breadcrumb">
+              <li><a href="index.html">Home</a></li>
+              <li class="active">my account</li>
+            </ol>
+          </div>
         </div>
-        <div class="col-lg-9 section--xl pt-0">
-          <div class="container">
-            <!-- User Info -->
-            <div class="row">
-              <!-- User Profile  -->
-              <div class="col-lg-12">
-                <div class="dashboard__user-profile dashboard-card">
-                  <div class="dashboard__user-profile-img">
-                    <img src="src/images/user/img-07.png" alt="userImg" />
-                  </div>
-                  <div class="dashboard__user-profile-info">
-                    <h5 class="font-body--xxl-500 name">{{ auth()->user()->name }}</h5>
-                    <p class="font-body--md-400 designation">Customer</p>
-                    <a
-                      href="account-setting.html"
-                      class="edit font-body--lg-500"
-                    >
-                    {{ auth()->user()->address }}
-                    </a>
-                  </div>
-                </div>
+      </div>
+    </div>
+  </section>
+  
+  
+  <section class="user-dashboard page-wrapper">
+    <div class="container">
+      <div class="row">
+        <div class="col-md-12">
+          <ul class="list-inline dashboard-menu text-center">
+            <li><a class="active" href="#">Profile</a></li>
+           
+          </ul>
+          <div class="dashboard-wrapper user-dashboard">
+            <div class="media">
+             
+              <div class="media-body">
+                <h2 class="media-heading">Welcome {{ auth()->user()->name }}</h2>
+             
               </div>
-              <!-- User Billing Address -->
-              
             </div>
-            <!-- Order History  -->
-            <div class="dashboard__order-history" style="margin-top: 24px">
-              <div class="dashboard__order-history-title">
-                <h2 class="font-body--xxl-500">Recent Order History</h2>
-                
-              </div>
-              <div class="dashboard__order-history-table">
-                <div class="table-responsive">
-                  <table class="table">
-                    <thead>
-                      <tr>
-                        <th
-                          scope="col"
-                          class="dashboard__order-history-table-title"
-                        >
-                          Order Id
-                        </th>
-                        <th
-                          scope="col"
-                          class="dashboard__order-history-table-title"
-                        >
-                          Date
-                        </th>
-                        <th
-                          scope="col"
-                          class="dashboard__order-history-table-title"
-                        >
-                          Price
-                        </th>
-                        <th
-                          scope="col"
-                          class="dashboard__order-history-table-title"
-                        >
-                          Status
-                        </th>
-                        <th
-                          scope="col"
-                          class="dashboard__order-history-table-title"
-                        ></th>
-                      </tr>
-                    </thead>
-                    <tbody>
-
-                        @if (empty($order))
+            <div class="total-order mt-20">
+              <h4>Address : {{ auth()->user()->address }}</h4>
+              <div class="table-responsive">
+                <table class="table">
+                  <thead>
+                    <tr>
+                      <th>Order ID</th>
+                      <th>Date</th>
+                      <th>Items</th>
+                      <th>Total Price</th>
+                      <th>Status</th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    @if (empty($order))
                         <tr>
                             <td colspan="8">No Order History</td>
                         </tr>
@@ -163,7 +57,7 @@
                         @foreach ($order as $index => $item)
                         <tr>
                             <th scope="row">{{ $index + 1 }}</th>
-                            <td>{{ $item->created_at }}</td>
+                            <td>{{ $item->created_at->format('Y-m-d') }}</td>
                             <td>{{ $item->price }} Tk</td>
 
                             <td>{{ $item->status }}</td>
@@ -172,15 +66,12 @@
                         </tr>
                         @endforeach
                         @endif
-                     
-                    </tbody>
-                  </table>
-                </div>
+                  </tbody>
+                </table>
               </div>
             </div>
           </div>
         </div>
       </div>
     </div>
-  </div>
-  <!-- dashboard Secton  End  -->
+  </section>
