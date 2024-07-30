be3c7ee5126aeff97c245b1f76e7356fa16b8321
zidankhan86 <zidankhan718@gmail.com>
2024-07-31 02:05:49 +0600

    fix: wishlist

--- resources/views/frontend/components/viewCart.blade.php
+++ resources/views/frontend/components/viewCart.blade.php
@@ -1,198 +1,48 @@
 
- <!-- breedcrumb section start  -->
- <div class="section breedcrumb">
-    <div class="breedcrumb__img-wrapper">
-      <img src="{{asset ('frontend/src/images/banner/breedcrumb.jpg') }}" alt="breedcrumb" />
-      <div class="container">
-        <ul class="breedcrumb__content">
-          <li>
-            <a href="index.html">
-              <svg
-                width="18"
-                height="19"
-                viewBox="0 0 18 19"
-                fill="none"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <path
-                  d="M1 8L9 1L17 8V18H12V14C12 13.2044 11.6839 12.4413 11.1213 11.8787C10.5587 11.3161 9.79565 11 9 11C8.20435 11 7.44129 11.3161 6.87868 11.8787C6.31607 12.4413 6 13.2044 6 14V18H1V8Z"
-                  stroke="#808080"
-                  stroke-width="1.5"
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                />
-              </svg>
-              <span> > </span>
-            </a>
-          </li>
-          <li class="active">
-            <a href="shopping-cart.html">Shopping cart</a>
-          </li>
-        </ul>
-      </div>
-    </div>
-  </div>
-  <!-- breedcrumb section end   -->
 
-  <!-- Shopping Cart Section Start   -->
-  <section class="shoping-cart section section--xl">
-    <div class="container">
-      <div class="section__head justify-content-center">
-        <h2 class="section--title-four font-title--sm">My Shopping Cart</h2>
-      </div>
+
+  <div class="page-wrapper">
+    <div class="cart shopping">
       @if(session()->has('cart') and count(session()->get('cart')) > 0)
-      <div class="row shoping-cart__content">
+      <div class="container">
+        <div class="row">
+          <div class="col-md-8 col-md-offset-2">
+            <div class="block">
+              <div class="product-list">
+                <form method="post">
+                  <table class="table">
+                    <thead>
+                      <tr>
+                        <th class="">Item Name</th>
+                        <th class="">Item Price</th>
+                        <th class="">Actions</th>
+                      </tr>
+                    </thead>
+                    <tbody>
+                      @php
+                      $subtotal = 0;
+                      @endphp
+                      @foreach(session()->get('cart') as $key => $data)
+                      @php
+                      $subtotal += $data['subtotal'];
+                      @endphp
 
-        <div class="col-lg-8">
-          <div class="cart-table">
-            <div class="table-responsive">
-              <table class="table">
-                <thead>
-                  <tr>
-                    <th scope="col" class="cart-table-title">Product</th>
-                    <th scope="col" class="cart-table-title">Price</th>
-                    <th scope="col" class="cart-table-title">quantity</th>
-                    <th scope="col" class="cart-table-title">Subtotal</th>
-                  </tr>
-                </thead>
-                <tbody>
-
-                            @php
-                            $subtotal = 0;
-                            @endphp
-                            @foreach(session()->get('cart') as $key => $data)
-                            @php
-                            $subtotal += $data['subtotal'];
-                            @endphp
-                  <tr>
-                    <!-- Product item  -->
-                    <td class="cart-table-item align-middle">
-                      <a
-                        href="product-details.html"
-                        class="cart-table__product-item"
-                      >
-                       
-                        <h5 class="font-body--lg-400">{{$data['name']}}</h5>
-                      </a>
-                    </td>
-                    <!-- Price  -->
-                    <td class="cart-table-item order-date align-middle">
-                        {{$data['price']}} Tk
-                    </td>
-                    <!-- quantity -->
-                    <td class="cart-table-item order-total align-middle">
-                      <div class="counter-btn-wrapper">
-                        
-                        <input
-                          type="readonly"
-                          id="counter-btn-counter"
-                          class="counter-btn-counter"
-                          min="1"
-                          max="1000"
-                          placeholder="1" value="{{$data['quantity']}}" disabled
-                        />
-                       
-                      </div>
-                    </td>
-                    <!-- Subtotal  -->
-                    <td class="cart-table-item order-subtotal align-middle">
-                      <div
-                        class="
-                          d-flex
-                          justify-content-between
-                          align-items-center
-                        "
-                      >
-                        <p class="font-body--md-500">{{$data['subtotal']}} Tk.</p>
-                        <a href="{{route('cart.item.delete',$key)}}">
-                        <button class="delete-item">
-                          <svg
-                            width="24"
-                            height="25"
-                            viewBox="0 0 24 25"
-                            fill="none"
-                            xmlns="http://www.w3.org/2000/svg"
-                          >
-                            <path
-                              d="M12 23.5C18.0748 23.5 23 18.5748 23 12.5C23 6.42525 18.0748 1.5 12 1.5C5.92525 1.5 1 6.42525 1 12.5C1 18.5748 5.92525 23.5 12 23.5Z"
-                              stroke="#CCCCCC"
-                              stroke-miterlimit="10"
-                            />
-                            <path
-                              d="M16 8.5L8 16.5"
-                              stroke="#666666"
-                              stroke-width="1.5"
-                              stroke-linecap="round"
-                              stroke-linejoin="round"
-                            />
-                            <path
-                              d="M16 16.5L8 8.5"
-                              stroke="#666666"
-                              stroke-width="1.5"
-                              stroke-linecap="round"
-                              stroke-linejoin="round"
-                            />
-                          </svg>
-                        </button>
-                    </a>
-                      </div>
-                    </td>
-                  </tr>
-                  
-                  @endforeach
-                </tbody>
-              </table>
-            </div>
-            <!-- Action Buttons  -->
-            
-              <div class="cart-table-action-btn d-flex">
-                <a
-                  href="{{ route('home') }}"
-                  class="button button--md  shop"
-                  >Return to Shop</a
-                >
-                <a href="{{route('cart.clear')}}" class="button button--md update"
-                  >Remove to Cart</a
-                >
-              </div>
-           
-          </div>
-
-       
-          
-        </div>
-
-        <div class="col-lg-4">
-          <div class="bill-card">
-            <div class="bill-card__content">
-              <div class="bill-card__header">
-                <h2 class="bill-card__header-title font-body--xxl-500">
-                  Order Summery
-                </h2>
-              </div>
-              <div class="bill-card__body">
-                <!-- memo  -->
-                <div class="bill-card__memo">
-                  <!-- Subtotal  -->
-                  <div class="bill-card__memo-item subtotal">
-                    <p class="font-body--md-400">Subtotal:</p>
-                    <span class="font-body--md-500">{{$data['subtotal']}} Tk.</span>
-                  </div>
-                  
-                  <!-- total  -->
-                  <div class="bill-card__memo-item total">
-                    <p class="font-body--lg-400">Total:</p>
-                    <span class="font-body--xl-500">{{$data['subtotal']}} Tk.</span>
-                  </div>
-                </div>
-                <form action="{{ url('/product-checkout', $key) }}">
-                  <button
-                    class="button button--lg w-100"
-                    style="margin-top: 20px"
-                    type="submit"
-                  >
-                    Place Order
-                  </button>
+                      <tr class="">
+                        <td class="">
+                          <div class="product-info">
+                            <img width="80" src="images/shop/cart/cart-1.jpg" alt="" />
+                            <a href="#!"> {{$data['name']}} Tk</a>
+                          </div>
+                        </td>
+                        <td class=""> {{$data['price']}} Tk</td>
+                        <td class="">
+                          <a class="product-remove" href="{{route('cart.item.delete',$key)}}">Remove</a>
+                        </td>
+                      </tr>
+                     @endforeach
+                    </tbody>
+                  </table>
+                  <a href="{{ url('/product-checkout', $key) }}" class="btn btn-main pull-right">Checkout</a>
                 </form>
               </div>
             </div>
@@ -200,12 +50,19 @@
         </div>
       </div>
       @else
-      <div class="row">
-          <div class="col-lg-12">
-              <h3>Your cart is empty. :)</h3>
+      <section class="empty-cart page-wrapper">
+        <div class="container">
+          <div class="row">
+            <div class="col-md-6 col-md-offset-3">
+              <div class="block text-center">
+                <i class="tf-ion-ios-cart-outline"></i>
+                  <h2 class="text-center">Your cart is currently empty.</h2>
+                
+                  <a href="{{ route('home') }}" class="btn btn-main mt-20">Return to shop</a>
+            </div>
           </div>
-      </div>
+        </div>
+      </section>
       @endif
     </div>
-  </section>
-  <!-- Shopping Cart Section End    -->
+  </div>
