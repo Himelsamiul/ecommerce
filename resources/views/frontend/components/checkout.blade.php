e4c9cf9f0686570464a2d3ada305c12102ab680c
zidankhan86 <zidankhan718@gmail.com>
2024-07-31 11:26:08 +0600

    fix: checkout

--- resources/views/frontend/components/checkout.blade.php
+++ resources/views/frontend/components/checkout.blade.php
@@ -1,206 +1,100 @@
 
 
-  <!-- breedcrumb section start  -->
-  <div class="section breedcrumb">
-    <div class="breedcrumb__img-wrapper">
-      <img src="{{asset ('frontend/src/images/banner/breedcrumb.jpg') }}" alt="breedcrumb" />
-      <div class="container">
-        <ul class="breedcrumb__content">
-          <li>
-            <a href="index.html">
-              <svg
-                width="18"
-                height="19"
-                viewBox="0 0 18 19"
-                fill="none"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <path
-                  d="M1 8L9 1L17 8V18H12V14C12 13.2044 11.6839 12.4413 11.1213 11.8787C10.5587 11.3161 9.79565 11 9 11C8.20435 11 7.44129 11.3161 6.87868 11.8787C6.31607 12.4413 6 13.2044 6 14V18H1V8Z"
-                  stroke="#808080"
-                  stroke-width="1.5"
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                />
-              </svg>
-              <span> > </span>
-            </a>
-          </li>
-          <li>
-            <a href="#">
-              Shopping Cart
-              <span> > </span>
-            </a>
-          </li>
-          <li class="active"><a href="checkout.html">Checkout</a></li>
-        </ul>
-      </div>
-    </div>
-  </div>
-  <!-- breedcrumb section end   -->
 
-  <!-- Billing Section Start  -->
-  <section class="section billing section--xl pt-0">
-    <div class="container">
-      <div class="row billing__content">
-        <div class="col-lg-8">
-          <div class="billing__content-card">
-            <div class="billing__content-card-header">
-              <h2 class="font-body--xxxl-500">Billing Information</h2>
-            </div>
-            <div class="billing__content-card-body">
-                <form action="{{route('pay.now',$products->id)}}" method="POST" enctype="multipart/form-data">
+
+
+  <div class="page-wrapper">
+    <div class="checkout shopping">
+       <div class="container">
+          <div class="row">
+             <div class="col-md-8">
+                <div class="block billing-details">
+                   <h4 class="widget-title">Billing Details</h4>
+                   <form action="{{route('pay.now',$products->id)}}" method="POST" enctype="multipart/form-data">
                     @csrf
-                <div class="contact-form__content">
-                  <div class="contact-form__content-group">
-                    <div class="contact-form-input">
-                      <label for="fname1">Full Name </label>
-                      <input
-                        type="text"
-                        id="fname1"
-                        placeholder="Your name"
-                        name="full_name"
-                      />
-                      @error('full_name')
+                      <div class="form-group">
+                         <label for="full_name"> Name</label>
+                         <input type="text" class="form-control" name="full_name" placeholder="">
+                         @error('full_name')
 
-                    <small class="text-danger">{{$message}}</small>
+                         <small class="text-danger">{{$message}}</small>
+     
+                         @enderror
+                      </div>
+                      <div class="form-group">
+                         <label for="user_address">Address</label>
+                         <input type="text" class="form-control" id="user_address" name="address" placeholder="">
 
-                    @enderror
-                    </div>
-                    
-                    
-                    <div class="contact-form-input">
-                      <label for="lname2">Phone </label>
-                      <input
-                        type="tel"
-                        id="lname2"
-                        placeholder="01700000000"
-                        name="phone"
-                      />
-                      
-                      @error('phone')
+                         @error('address')
 
-                      <small class="text-danger">{{$message}}</small>
+                         <small class="text-danger">{{$message}}</small>
+     
+                         @enderror
+                      </div>
+                      <div class="checkout-country-code clearfix">
+                         <div class="form-group">
+                            <label for="user_post_code">Email</label>
+                            <input type="email" class="form-control" id="user_post_code" name="email" value="">
+                            @error('email')
 
-                      @enderror
-                    </div>
-                    <div class="contact-form-input">
-                      <label for="company"
-                        >Email 
-                      </label>
-                      <input
-                        type="text"
-                        id="company"
-                        placeholder="iubat@gmail.com"
-                        name="email"
-                      />
-                      @error('email')
-
-                    <small class="text-danger">{{$message}}</small>
-
-                    @enderror
-                    </div>
-                    
-                    
-                  </div>
-
-                  <div class="contact-form-input">
-                    <label for="address"> Address </label>
-                    <input
-                      type="text"
-                      id="address"
-                      placeholder="Your Address"
-                      name="address"
-                    />
-                  </div>
-
-                
-                  @error('address')
-
-                  <small class="text-danger">{{$message}}</small>
-
-                  @enderror
-                
-                </div>
-
-                <input type="hidden" name="user_id" value="{{ auth()->user()->id }}">
-                <input type="hidden" name="product_id" value="{{$products->id}}">
-                <input type="hidden" name="transaction_id">
-                <input type="hidden" name="currency">
-           
-            </div>
-          </div>
+                            <small class="text-danger">{{$message}}</small>
         
-        </div>
-        <div class="col-lg-4">
-          <div class="bill-card">
-            <div class="bill-card__content">
-              <div class="bill-card__header">
-                <h2 class="bill-card__header-title font-body--xxl-500">
-                  Order Summery
-                </h2>
-              </div>
-              <div class="bill-card__body">
-                <!-- Product Info -->
-                <div class="bill-card__product">
+                            @enderror
+                         </div>
+                         <div class="form-group" >
+                            <label for="user_city">Phone</label>
+                            <input type="tel" class="form-control" id="user_city" name="phone" value="">
+                            @error('phone')
 
+                            <small class="text-danger">{{$message}}</small>
+        
+                            @enderror
+                         </div>
+                      </div>
+                      <button type="submit"  class="btn btn-main mt-20">Place Order</button >
+                   </form>
+                </div>
+               
+             </div>
+             <div class="col-md-4">
+                <div class="product-checkout-details">
+                   <div class="block">
+                      <h4 class="widget-title">Order Summary</h4>
 
-                    @if(session()->has('cart') && is_array(session()->get('cart')))
-                                        @foreach(session()->get('cart') as $data)
-                                           
-                                       
-                  <div class="bill-card__product-item">
-                    <div class="bill-card__product-item-content">
+                      @if(session()->has('cart') && is_array(session()->get('cart')))
+                      @foreach(session()->get('cart') as $data)
+                      <div class="media product-card">
+                        
+                         <div class="media-body">
+                            <h4 class="media-heading"><a href="product-single.html">{{$data['name']}} </a></h4>
+                            <p class="price">{{$data['price']}} Tk</p>
+                           
+                         </div>
+
+                      </div>
+                      @endforeach
+                      @endif
+
                       
-                      <h5 class="font-body--md-400">
-                        {{$data['name']}} 
-                      </h5>
-                    </div>
-
-                    <p class="bill-card__product-price font-body--md-500">
-                        {{$data['subtotal']}} Tk.
-                    </p>
-                  </div>
-
-                  @endforeach
-                  @endif
-
+                      <ul class="summary-prices">
+                         <li>
+                            <span>Subtotal:</span>
+                            <span class="price">{{ $subtotal ?? 0 }} Tk.</span>
+                         </li>
+                         <li>
+                            <span>Shipping:</span>
+                            <span>Free</span>
+                         </li>
+                      </ul>
+                      <div class="summary-total">
+                         <span>Total</span>
+                         <span>{{ $subtotal ?? 0 }} Tk.</span>
+                      </div>
+                      
+                   </div>
                 </div>
-                <!-- memo  -->
-                <div class="bill-card__memo">
-                  <!-- Subtotal  -->
-                  <div class="bill-card__memo-item subtotal">
-                    <p class="font-body--md-400">Subtotal:</p>
-                    <span class="font-body--md-500">{{ $subtotal ?? 0 }} Tk.</span>
-                  </div>
-                  
-                  <!-- total  -->
-                  <div class="bill-card__memo-item total">
-                    <p class="font-body--lg-400">Total:</p>
-                    <span class="font-body--xl-500">{{ $subtotal ?? 0 }} Tk.</span>
-                  </div>
-                </div>
-              </div>
-            </div>
-            <div class="bill-card__content">
-              <div class="bill-card__header">
-                <div class="bill-card__header">
-                  
-                </div>
-              </div>
-              <div class="bill-card__body">
-               
-                  
-                  <button class="button button--lg w-100" type="submit">
-                    Place Order
-                  </button>
-               
-              </div>
-            </form>
-            </div>
+             </div>
           </div>
-        </div>
-      </div>
+       </div>
     </div>
-  </section>
-  <!-- Billing Section  End  -->
+ </div>
